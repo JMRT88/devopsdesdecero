14709774ab89f58c980d434840a40f903ecfa0e0
Oscar Marq Olv <oscar.marq.olv@gmail.com>
2021-10-30 13:43:02 -0500

    Changes in `main.sh`

--- main.sh
+++ main.sh
@@ -1,3 +1,19 @@
 #!/bin/bash -
 
 sleep 10
+echo "aqui se está detonando la tarea"
+
+sleep 10
+echo "lanzando pruebas unitarias"
+
+sleep 10
+echo "generando dump de base de datos"
+
+sleep 10
+echo "lanzando pruebas de estres al servidor"
+
+sleep 10 
+echo "lanzando contenedores docker adicionales..."
+
+slepp 10
+echo "finalizando pruebas..."
